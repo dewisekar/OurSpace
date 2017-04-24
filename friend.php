e1f77109b54d2a2aec1b63e09d5586f8a2715cca
ivanderwilliam <ivanderwilliam@gmail.com>
2017-04-24 13:48:37 +0700

    Init project

---
+++ friend.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LBE - Profile</title>
+    <link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet">
+</head>
+<style>
+            input[type=text] {
+    width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('searchicon.png');
+    background-position: 10px 10px; 
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+
+input[type=text]:focus {
+    width: 100%;
+}
+    </style>
+<body>
+
+    <div class="row" style="background-image: url('assets/qwe.jpg');">
+        <div class="container">
+            <div class="col-sm-2">
+            </div>
+                <div class="col-sm-8">
+
+<form>
+  <input type="text" name="search" placeholder="Search..">
+</form>
+
+            <div class="panel panel-default" style="background-color:#e5e05e">
+                    <div class="panel-heading">
+                        <b>Search Result</b>
+                    </div>
+                    <div class="panel-body">
+                        <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                        Jacob <br>
+                        <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                        James <br>
+                    </div>
+                    
+                </div>
+            </div>
+    <script src="assets/js/jquery-3.2.0.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+    
+</html>
---
+++ profile.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LBE - Profile</title>
+    <link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <style>
+    </style>
+</head>
+    
+<body>
+    
+    <div class="row" style="background-image: url('assets/qwe.jpg'); padding-top:5rem">
+        <div class="container">
+            <div class="col-sm-3">
+                <div class="panel panel-default" style="background-color:#e5e05e">
+                    <div class="panel-heading">
+                        <b>Profile</b>
+                    </div>
+                    <div class="panel-body">
+                        <img src="assets/qwe.jpg" style="width:100%; margin-bottom:1rem">
+                        <a href="https://www.google.com">
+                        <img src="assets/Facebook.png" width="32" height="32" border="0">
+                        </a>
+                        <a href="https://www.google.com">
+                        <img src="assets/Twitter.png" width="32" height="32" border="0">
+                        </a>
+                        <a href="https://www.google.com">
+                        <img src="assets/gp.png" width="32" height="32" border="0">
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="col-sm-9" style="color:#fff">
+                <h1>Ivander William</h1>
+                <b>@ivander</b>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">OurSpace</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Inbox</a></li>
+      <li><a href="#">Notification</a></li>
+      <li><a href="#">Log Out</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+    <div class="row" style="margin-top:5rem">
+        <div class="container">
+            <div class="col-sm-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>Friends</b>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                        <div class="container">
+                            <div class="col-sm-4">
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            </div>
+                            </div>
+                        <button type="button" class="btn btn-primary" style=margin-left:1rem>Add Friend</button>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>Gallery</b>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                        <div class="container">
+                            <div class="col-sm-4">
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            <img src="assets/qwe.jpg" width="60" height="60" border="1" style=margin-bottom:1rem>
+                            </div>
+                            </div>
+                        <button type="button" class="btn btn-primary" style=margin-left:1rem>See More</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>Description</b>
+                    </div>
+                    <div class="panel-body">
+                        WOWOWOOWOWOWOWOWO
+                    </div>
+                    
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>Timeline
+</b>
+                    </div>
+                    <div class="panel-body">
+                        <p>04/17/2017</p>
+                        My first post
+                        <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>Comment 0 Likes<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.fa {
+    font-size: 25px;
+    cursor: pointer;
+    user-select: none;
+}
+
+.fa:hover {
+  color: darkblue;
+}
+</style>
+</head>
+<body>
+
+
+<i onclick="myFunction(this)" class="fa fa-thumbs-up"></i   >
+
+<script>
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+}
+</script>
+
+</body>
+</html></b>
+                    </div>
+                    <div class="panel-body">
+                        hrfuerfu
+                    </div>
+                
+                </div>
+                
+                    </div>
+                
+                </div>
+            
+            </div>
+        <div class="col-sm-3">
+ 
+<form>
+  <input type="text" name="search" placeholder="Search..">
+</form>
+<br>
+        
+</div>
+
+    
+    <script src="assets/js/jquery-3.2.0.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+</div>
